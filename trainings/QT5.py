99616b9af9d1bcb28c39564abc84339ddec0146b
ParsaPNT128 <papilevary@gmail.com>
2024-08-02 11:47:50 +0330

    updated

--- trainings/QT5.py
+++ trainings/QT5.py
@@ -1,46 +1,44 @@
 answers = []
-messages = ['Peygir', 'Tannaz']
-n = 1
+k = []
 
-def message_tracker(k, messages):
-    if k == len(messages):
-        return messages[k - 1]
+def message_tracker(k, t):
+    messages = ['Peygir', 'Tannaz']
+    n = 1
+    for i in range(k):
+        if messages[len(messages) - 1] == 'Morshed':
+            if n != t:
+                messages.append('Tannaz')
+            else:
+                messages.append('Peygir')
+                messages.append('Tannaz')
+                n = 0
+        elif messages[len(messages) - 1] == 'Tannaz':
+            if n != t:
+                messages.append('Jeddy')
+            else:
+                messages.append('Peygir')
+                messages.append('Jeddy')
+                n = 0
+        elif messages[len(messages) - 1] == 'Jeddy':
+            if n != t:
+                messages.append('Morshed')
+            else:
+                messages.append('Peygir')
+                messages.append('Morshed')
+                n = 0
+
+        n += 1
     
-    minus = int(k / len(messages))
-    k = k - minus * len(messages)
-    return messages[k - 1]
-
-
+    return messages
+    
 q, t = input().split()
-
-for i in range(int(t)*3-1):
-    if messages[len(messages) - 1] == 'Morshed':
-        if n != int(t):
-            messages.append('Tannaz')
-        else:
-            messages.append('Peygir')
-            messages.append('Tannaz')
-            n = 0
-    elif messages[len(messages) - 1] == 'Tannaz':
-        if n != t:
-            messages.append('Jeddy')
-        else:
-            messages.append('Peygir')
-            messages.append('Jeddy')
-            n = 0
-    elif messages[len(messages) - 1] == 'Jeddy':
-        if n != t:
-            messages.append('Morshed')
-        else:
-            messages.append('Peygir')
-            messages.append('Morshed')
-            n = 0
-
-    n += 1
-
 for i in range(int(q)):
-    k = int(input())
-    answers.append(message_tracker(k , messages))
+    k.append(int(input()))
+
+messages = message_tracker(max(k) , int(t))
+
+for i in k:
+    answers.append(messages[i - 1]) 
 
 for i in answers:
     print(i)
