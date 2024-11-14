753b1c156a84e75d3edd0ec14dbedc93d75c7f24
ParsaPNT128 <papilevary@gmail.com>
2024-11-14 23:35:27 +0330

    updated

--- trainings/QT20.py
+++ trainings/QT20.py
@@ -10,8 +10,19 @@ def func(houses, s, t):
         elif houses[i] == "H":
             k += 1
         elif k != 0:
-            if is_power_of_two(k):
-                o += 1
+            searching = True
+            rest = 0
+            while searching:
+                if is_power_of_two(k):
+                    o += 1
+                    if rest == 0:
+                        searching = False
+                    else:
+                        k = rest
+                        rest = 0
+                else:
+                    k -= 1
+                    rest += 1
             k = 0
 
     return o
